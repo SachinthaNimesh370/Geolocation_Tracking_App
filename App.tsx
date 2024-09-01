99196e66d16c22b3a9f440c13beb5c36c9d612c4
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2024-09-01 12:13:38 +0530

    add the Permission code

--- App.tsx
+++ App.tsx
@@ -7,6 +7,28 @@ import { Text, View,PermissionsAndroid,TouchableOpacity } from 'react-native';
 
 
 function App(): React.JSX.Element {
+
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Cool Photo App Location Permission',
+          message:'Cool Photo App needs access to your location ' ,  
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the location');
+      } else {
+        console.log('Location permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
   
 
   return (
